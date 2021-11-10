42f9c856e6929af6115bb88fb2c9724487c28018
seenso <>
2021-11-10 13:52:32 -0900

    Passed Challenge 1

--- src/index.js
+++ src/index.js
@@ -1,1 +1,25 @@
 console.log('%c HI', 'color: firebrick')
+
+const imgUrl = "https://dog.ceo/api/breeds/image/random/4";
+const breedUrl = "https://dog.ceo/api/breeds/list/all";
+
+document.addEventListener("DOMContentLoaded", () => {
+  //CHALLENGE 1
+  fetch(imgUrl)
+  .then(res => res.json())
+  .then(images => {
+    // img.message holds arr of img urls
+    images.message.forEach(imageURL => {
+      //create node
+      let imgNode = document.createElement("img");
+      imgNode.src = imageURL;
+      document.getElementById("dog-image-container").appendChild(imgNode);
+    });
+  });
+
+  //CHALLENGE 2
+
+
+
+
+});
